4a3d8ac689af364de0009fafd128119510097167
Sercan Eraslan <sercan.eraslan@sahibinden.com>
2014-12-16 18:22:58 +0200

    angular disqus directive

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-angular-disqus-directive
-========================
+<img align="center" height="108" src="https://www.angularjs.org/img/AngularJS-large.png">
 
 Angular Disqus Directive
+========================
+
+Angular directive for embeding <a href="http://disqus.com">Disqus</a> comments on your site.
+
+Getting Started
+========================
+
+Minimum Usage:
+
+```
+<disqus shortname="your-disqus-shortname"></disqus>
+```
+
+Advanced Usage:
+
+```
+<disqus shortname="your-disqus-shortname"
+        identifer="your-identifer-id"
+        title="your-page-title"
+        url="your-page-url"
+        categoryId="your-category-id"
+></disqus>
+```
+
+Documentation
+========================
+
+Please read the <a href="https://help.disqus.com/customer/portal/articles/472098-javascript-configuration-variables">full documentation</a>.
+
+
+Contributing
+========================
+
+Feel free to open an issue or send a pull request.
+
+License
+========================
+
+MIT License.
+
+Author
+========================
+
+Copyright © 2014 Sercan Eraslan
---
+++ ng-disqus.js
@@ -0,0 +1,41 @@
+/*
+*
+* ng-disqus v0.0.1
+*
+* (c) 2014-2015 Sercan Eraslan http://sercaneraslan.com
+* License: MIT
+*
+*/
+angular.module('ngDisqus', []).
+    directive('disqus', function($window) {
+
+        return {
+            restrict: 'E',
+            scope: {
+                'shortname': '@',
+                'identifier': '@',
+                'title': '@',
+                'url': '@',
+                'categoryId': '@'
+            },
+            template: '<div id="disqus_thread" class="disqus"></div>',
+            link: function(scope) {
+
+                // Disqus Global Variables
+                $window.disqus_shortname = scope.shortname;
+                $window.disqus_identifier = scope.identifier;
+                $window.disqus_title = scope.title;
+                $window.disqus_url = scope.url;
+                $window.disqus_category_id = scope.categoryId;
+
+                // Create Disqus Script
+                var dsq = document.createElement('script');
+
+                dsq.type = 'text/javascript';
+                dsq.async = true;
+                dsq.src = document.location.protocol + '//' + scope.shortname + '.disqus.com/embed.js';
+
+                document.getElementsByTagName('body')[0].appendChild(dsq);
+            }
+        };
+    });
